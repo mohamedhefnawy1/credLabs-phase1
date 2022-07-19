e97a34ca7467b878bc92709c3281c5d86f9c624c
mohamedhefnawy1 <mohamedhefnawy10@gmail.com>
2022-07-19 01:11:01 -0600

    fixing images

--- CREDLABS-landing-page/script.js
+++ CREDLABS-landing-page/script.js
@@ -4,7 +4,8 @@ const creatorButton = document.getElementById("creatorButton");        // create
             console.log("The button was clicked");                             
             // const name = prompt("Enter name: ");
             // console.log(name);
-            location.href = "http://127.0.0.1:5500/CREDLABS-creators-page/index.html";
+            // location.href = "http://127.0.0.1:5500/CREDLABS-creators-page/index.html";
+            location.href = "../CREDLABS-creators-page/index.html";
             
 
         });
@@ -15,5 +16,6 @@ const expertButton = document.getElementById("expertButton");        // create a
             console.log("The button was clicked");                             
             // const name = prompt("Enter name: ");
             // console.log(name);
-            location.href = "http://127.0.0.1:5500/CREDLABS-experts-page/index.html";  // need the link for the expert page
+            // location.href = "http://127.0.0.1:5500/CREDLABS-experts-page/index.html";  // need the link for the expert page
+            location.href = "../CREDLABS-experts-page/index.html";
         });
