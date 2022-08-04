b0fe2a85df215f0cc2d5758dc23cbe2e94ccf0f1
mohamedhefnawy1 <mohamedhefnawy10@gmail.com>
2022-08-04 13:32:06 -0700

    fixing bodyHolder

--- authorityGenius-landing-page/script.js
+++ authorityGenius-landing-page/script.js
@@ -20,9 +20,9 @@ const navButton = document.getElementById("navButton");        // create a const
             location.href = "https://effulgent-croquembouche-26c9f4.netlify.app/";
         });
 
-const expertButton = document.getElementById("expertButton");        // create a constant in js file that references the button we made above
+const joinButton = document.getElementById("joinButton");        // create a constant in js file that references the button we made above
         //console.log(creatorButton);                                          // logs the value of the constant "creatorButton" to the console (dev tools)
-        navButton.addEventListener("click", function(e) {                  // this is for what happens when the button we created is clicked 
+        joinButton.addEventListener("click", function(e) {                  // this is for what happens when the button we created is clicked 
         console.log("The button was clicked");                             
         // const name = prompt("Enter name: ");
         // console.log(name);
